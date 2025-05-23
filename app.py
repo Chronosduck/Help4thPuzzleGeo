f238c7c9ccc362bc799e2f1c7e7ac9e0c203f510
RondoPuffinXY <140180068+Chronosduck@users.noreply.github.com>
2025-05-23 19:58:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 
 app = Flask(__name__)
 
@@ -7,11 +7,18 @@ def index():
     result = ""
     if request.method == "POST":
         user_input = request.form["input"]
-        # Logic ของคุณ
-        result = f"ผลลัพธ์ของคุณคือ: {user_input[::-1]}"  # แค่ reverse ตัวอย่าง
+        if user_input == "AAAABBCCDDD":
+            return redirect(url_for("s1"))
+        elif user_input.lower() == "aaaabbccddd":
+            result = "กรุณาพิมพ์ตัวใหญ่เท่านั้น"
+        else:
+            result = "รูปแบบ input ผิด กรุณาลองใหม่"
     return render_template("index.html", result=result)
+
+@app.route("/s1")
+def s1():
+    return render_template("s1.html")
 
 if __name__ == "__main__":
     app.run(debug=False, host="0.0.0.0", port=10000)
-
 
